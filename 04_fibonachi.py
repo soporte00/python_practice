62c8770db23cfd0a757e3f52dff7f949c3bdb62d
David <soporte00@gmail.com>
2021-09-04 22:29:51 -0300

    add comment

--- 04_fibonachi.py
+++ 04_fibonachi.py
@@ -5,3 +5,12 @@ while a <= 1597:
 	c=a+b
 	a = b
 	b = c	
+
+
+
+'''	
+	crear un programa que entrege números de fibonachi 
+	del 0 al 1597, se tienen que mostrar en una sola linea 
+	y no debe ocupar más de 7 lineas de código
+
+'''
